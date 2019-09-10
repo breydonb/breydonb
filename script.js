0a396684621b3a4acf911118b280b3bf7886fd64
breydonb <54943620+breydonb@users.noreply.github.com>
2019-09-09 22:20:50 -0500

    Add files via upload
    before it's due

---
+++ script.js
@@ -0,0 +1,72 @@
+var d = new Date();
+document.getElementById("demo").innerHTML = d;
+
+function buyGameFn9(){
+	
+	window.open("https://www.amazon.com/Legend-Zelda-Breath-Wild-Nintendo-Switch/dp/B01MS6MO77/ref=sr_1_3?keywords=breath+of+the+wild&qid=1567968933&s=gateway&sr=8-3")
+	
+	
+}
+
+function buyGameFn8(){
+	
+	window.open("https://store.steampowered.com/app/612880/Wolfenstein_II_The_New_Colossus/")
+	
+	
+}
+
+function buyGameFn7(){
+	
+	window.open("https://www.amazon.com/Mario-Kart-8-Deluxe-Nintendo-Switch/dp/B01N1037CV/ref=sr_1_1?keywords=mario+kart+8+deluxe&qid=1567971494&s=gateway&sr=8-1")
+	
+	
+}
+
+function buyGameFn6(){
+	
+	window.open("https://www.origin.com/usa/en-us/store/battlefield/battlefield-v")
+	
+	
+}
+
+function buyGameFn5(){
+	
+	window.open("https://store.steampowered.com/app/289070/Sid_Meiers_Civilization_VI/")
+	
+	
+}
+
+function buyGameFn4(){
+	
+	window.open("https://www.amazon.com/Legend-Zelda-Breath-Wild-Nintendo-Switch/dp/B01MS6MO77/ref=sr_1_3?keywords=breath+of+the+wild&qid=1567968933&s=gateway&sr=8-3")
+	
+	
+}
+
+function buyGameFn3(){
+	
+	window.open("https://www.amazon.com/Legend-Zelda-Breath-Wild-Nintendo-Switch/dp/B01MS6MO77/ref=sr_1_3?keywords=breath+of+the+wild&qid=1567968933&s=gateway&sr=8-3")
+	
+	
+}
+
+function buyGameFn2(){
+	
+	window.open("https://www.amazon.com/Legend-Zelda-Breath-Wild-Nintendo-Switch/dp/B01MS6MO77/ref=sr_1_3?keywords=breath+of+the+wild&qid=1567968933&s=gateway&sr=8-3")
+	
+	
+}
+
+function buyGameFn1(){
+	
+	window.open("https://www.amazon.com/Legend-Zelda-Breath-Wild-Nintendo-Switch/dp/B01MS6MO77/ref=sr_1_3?keywords=breath+of+the+wild&qid=1567968933&s=gateway&sr=8-3")
+	
+	
+}
+
+function buyGameFn0(){
+	
+	window.open("https://www.amazon.com/Legend-Zelda-Breath-Wild-Nintendo-Switch/dp/B01MS6MO77/ref=sr_1_3?keywords=breath+of+the+wild&qid=1567968933&s=gateway&sr=8-3")
+	
+	
+}
