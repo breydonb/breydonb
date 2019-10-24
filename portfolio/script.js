d0ec1d72daf523a866405674514081713027126c
breydonb <54943620+breydonb@users.noreply.github.com>
2019-10-24 09:13:41 -0500

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,41 @@
+$('#hvritem1,#item1').mouseenter(function(){
+  $('#hvritem1').show();
+});
+$('.item,#item1').mouseleave(function(){
+  $('#hvritem1').hide();
+});
+
+$('#hvritem2,#item2').mouseenter(function(){
+  $('#hvritem2').show();
+});
+$('.item,#item2').mouseleave(function(){
+  $('#hvritem2').hide();
+});
+
+$('#hvritem3,#item3').mouseenter(function(){
+  $('#hvritem3').show();
+});
+$('.item,#item3').mouseleave(function(){
+  $('#hvritem3').hide();
+});
+
+$('#hvritem4,#item4').mouseenter(function(){
+  $('#hvritem4').show();
+});
+$('.item,#item4').mouseleave(function(){
+  $('#hvritem4').hide();
+});
+
+$('#hvritem5,#item5').mouseenter(function(){
+  $('#hvritem5').show();
+});
+$('.item,#item5').mouseleave(function(){
+  $('#hvritem5').hide();
+});
+
+$('#hvritem6,#item6').mouseenter(function(){
+  $('#hvritem6').show();
+});
+$('.item,#item6').mouseleave(function(){
+  $('#hvritem6').hide();
+});
