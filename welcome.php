5b5d957366d9197361a6cdfee78a6b4f7b6e0aa9
Breydon Brennan -Aug-17-2018 <bbrennan9@bcst01.parkland.edu>
2020-03-30 13:09:03 -0500

    Adding the welcome page to my main repository...

---
+++ welcome.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Welcome to breydonb's GitHub</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="container">
+		<h1>Welcome to My Repository!</h1>
+	</div>
+	<br />
+	<div class="flex-menu">
+		<img src="https://cdn.mos.cms.futurecdn.net/42E9as7NaTaAi4A6JcuFwG-320-80.jpg" alt="banana you can't see">
+		<p>Eventually, I will have links for you to press. Until then, please enjoy this picture of a banana</p>
+	</div>
+	
+</body>
+</html>
