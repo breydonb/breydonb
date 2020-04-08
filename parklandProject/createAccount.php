8305646d18b54488d0fb80d80746208f72a1d232
Breydon Brennan -Aug-17-2018 <bbrennan9@bcst01.parkland.edu>
2020-04-08 12:43:18 -0500

    Finishing up createAccount.php

--- parklandProject/createAccount.php
+++ parklandProject/createAccount.php
@@ -13,47 +13,51 @@ function databaseConnection(){
 	$database= "bbrennan9";
 	
 	$conn = mysqli_connect("localhost",$user,$password,$database);
-	if (mysqli_connect_errno()) {                                                        
-        die("<b>Failed to connect to MySQL: " . mysqli_connect_error() . "</b>");        
-    }                                                                                    
-                                                                                         
-    return $conn;
+	if (mysqli_connect_errno()) {
+		die("<b>Failed to connect to MySQL: " . mysqli_connect_error() . "</b>");   
+		}
+                 
+    	return $conn;
 
-	if (isset($_POST['selection'])) // form loaded itself
+	if (isset($_POST['submit'])) // form loaded itself
 	{
+	    if ($_POST['submit'] == "Create Account") // insert new record chosen
+		{
+       			$stmt = $conn -> prepare("INSERT INTO users (usrname,encrypted_password,usergroup,email) VALUES (?,?,?,?)");
+			$stmt -> bind_param("ssss",$usrname,$encrypted_password, $usergroup,$email);
+			$usrname= $_POST['usrname'];
+			$encrypted_password=$_POST['passwd'];
+			$usergroup="user";
+			$email="none set";
+			
+			$stmt -> execute();
 
-    if ($_POST['selection'] == "Create Account") // insert new record chosen
-    {
-        // store the record (next slide) 
-        header("Location: welcome.php");
-    }
-    if ($_POST['selection'] == "Cancel") // insert new record chosen
-    {
-        header("Location: welcome.php");
-    }
+			header("Location: welcome.php");
+    		}
+    		if ($_POST['submit'] == "Cancel") // insert new record chosen
+    		{
+        		header("Location: welcome.php");
+    		}
 	}
-
-	
 }
 
 
 ?>
 </head>
-
 <body>
 	<div class="login-box">
 		<form method="POST">
 			<label>Username:</label>
-			<input type="text" name="Username">
+			<input type="text" name="usrname">
 			<br>
 			<label>Password:</label>
-			<input type="password" id="passwd">
+			<input type="password" name="passwd" id="passwd">
 			<br>
 			<label>Verify Password:</label>
 			<input type="password" id="verifyPasswd">
 			<br>
-			<input type="submit" value="Create Account">
-			<input type="submit" value="Cancel">
+			<input type="submit" name="submit" value="Create Account">
+			<input type="submit" name="submit" value="Cancel">
 		</form>
 	</div>
 </body>
