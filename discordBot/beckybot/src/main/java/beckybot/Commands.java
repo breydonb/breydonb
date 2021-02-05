66ba881ed66974d3a99ebd11f1b588fa39f4567f
breydonb <54943620+breydonb@users.noreply.github.com>
2021-02-04 18:59:07 -0600

    Update Commands.java
    Finishing up embedded help command. Awaiting testing

--- discordBot/beckybot/src/main/java/beckybot/Commands.java
+++ discordBot/beckybot/src/main/java/beckybot/Commands.java
@@ -25,7 +25,18 @@ class Commands{
                 try{
                     MessageChannel channel = sMessage.getChannel().block();
                     channel.createEmbed(spec->
-                        spec.setColor(Color.red)
+                        spec.setColor(Color.red).setAuthor("setAuthor", "https://www.google.com/", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AIcon_Bird_512x512.png&psig=AOvVaw3n3yUoXTJYD-B0e1H1bppw&ust=1612573008132000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOjc1NzE0e4CFQAAAAAdAAAAABAD").setImage(IMAGE_URL)
+                        .setTitle("setTitle/setUrl")
+                        .setUrl(ANY_URL)
+                        .setDescription("setDescription\n" +
+                          "big D: is setImage\n" +
+                          "small D: is setThumbnail\n" +
+                          "<-- setColor")
+                        .addField("addField", "inline = true", true)
+                        .addField("addFIeld", "inline = true", true)
+                        .addField("addFile", "inline = false", false)
+                        .setThumbnail(IMAGE_URL)
+                        .setFooter("setFooter --> setTimestamp", https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AIcon_Bird_512x512.png&psig=AOvVaw3n3yUoXTJYD-B0e1H1bppw&ust=1612573008132000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOjc1NzE0e4CFQAAAAAdAAAAABAD)
                     ).block();
 
                 }catch(NullPointerException npe){
