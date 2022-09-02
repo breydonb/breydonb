0fcaee02a644f288cbbc12e0551abca773d3baa5
breydonb <spyvsspy1113@gmail.com>
2022-09-02 01:54:11 -0500

    Adding error checking

--- React/portfolio-website/src/components/blog/Blog.jsx
+++ React/portfolio-website/src/components/blog/Blog.jsx
@@ -6,49 +6,43 @@ import '../../App.css';
 
 import { API_VAR } from '../../api-var';
 import { Button, Badge} from 'react-bootstrap';
+import axios from 'axios';
 
 
 function Blog(){
     const [ data, setData ] = useState([]);
-    const [ error, setErrorCode ] = useState([""])
 
     useEffect( () => {
-        loadBlogs();
-        // getData();
+        axios.get("https://localhost:44371/api/blog")
+            .then(res => {
+                // console.log(res.data)
+                setData(res.data)
+            })
+            .catch(function (err) {
+                if(err.response) {
+                    console.log(err.response.data);
+                    console.log(err.response.status);
+                    console.log(err.response.headers);
+                }
+                else{
+                    console.log(err)
+                }
+            })
     }, [])
-
-    const loadBlogs = async () => {
-        try{
-            await fetch(API_VAR.BLOG_URL)
-                .then(response => response.json())
-                .then(receivedData => setData(receivedData))
-        }
-        catch(e){
-            console.error(e)
-        }
-    }
-
-    if(data.length === 0){
-        return(
-            <div className='container'>
-                <p className='text-center'>Error Reading the Data</p>
-            </div>
-        )
-    }
 
     return(
         <div className='blog-container'>
             {data.map(post =>(
-                    <div className='blog-card'>
+                    <div className='blog-card' key={post.BlogId}>
                         <img src="img/ph.svg" className='card-img' width='600'/>
-                        <div className='card-body'>
+                        <div className='card-body p-2'>
                             <div className=''>
                                 <Badge pill bg="primary">{post.PostCategory}</Badge>
                                 <h4>{post.Title}</h4>
                             </div>
                             <p>{post.BlogDesc}</p>
                         </div>
-                        <div className='card-footer d-flex justify-content-between align-items-center'>
+                        <div className='card-footer d-flex justify-content-between align-items-center p-2'>
                             <small>{post.PublishDate}</small>
                             <Button 
                                 variant="primary">Read More</Button>
